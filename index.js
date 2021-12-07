dc7bec90716d4e7717d8820c1d4313b41c064933
Venkat Anirudh S N <venkat.anirudh12@gmail.com>
2021-12-07 08:43:47 +0530

    simple node js project with modules

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ const fs = require("fs");
 //Server
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //read templates
 const tempOverview = fs.readFileSync(
     `${__dirname}/templates/template-overview.html`,
@@ -39,20 +41,6 @@ const tempOverview = fs.readFileSync(
     `${__dirname}/templates/template-product.html`,
     'utf-8'
   );
-
-const replaceTemplate =(temp,product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-}
 
 //read File synchronously for api
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
